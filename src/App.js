1c831d45bfa27796748da955a645e7bd68d1f3ac
Tommy Nguyen <tommy@Tommys-MacBook-Pro.local>
2020-09-02 15:01:30 -0400

    Changement dans App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+
+  console.log("Changes for GitHub practice");
+
   return (
     <div className="App">
       <header className="App-header">
